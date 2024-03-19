92a5420b52df96ef715449dce77f1d4a05b35916
CoeJoder <CoeJoder@users.noreply.github.com>
2024-03-19 00:49:07 -0700

    Update deploy script

--- scripts/deploy_setup_unix-UbuntuDesktop_23_10.sh
+++ scripts/deploy_setup_unix-UbuntuDesktop_23_10.sh
@@ -2,14 +2,18 @@
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 PROJ_DIR="$(readlink -f "$SCRIPT_DIR/../")"
 
-echo "Deploying the dotfiles..."
-rsync -avh $PROJ_DIR/ ~
-
-echo "Overriding with private configs..."
-GIT_CONFIG=~/.config/git/config
-GIT_CONFIG_PRIV=${GIT_CONFIG}.priv
-if [ -f $GIT_CONFIG_PRIV ]; then
-    cp -v $GIT_CONFIG_PRIV $GIT_CONFIG
+if [ ~ -ef $PROJ_DIR ]; then
+    printf "This script is intended to be run from the cloned dir, not the deployment destination.\nExiting...\n"
 else
-    echo "$GIT_CONFIG_PRIV not found, skipping..."
+    echo "Deploying the dotfiles..."
+    rsync -avh $PROJ_DIR/ ~
+
+    echo "Overriding with private configs..."
+    GIT_CONFIG=~/.config/git/config
+    GIT_CONFIG_PRIV=${GIT_CONFIG}.priv
+    if [ -f $GIT_CONFIG_PRIV ]; then
+        cp -v $GIT_CONFIG_PRIV $GIT_CONFIG
+    else
+        echo "$GIT_CONFIG_PRIV not found, skipping..."
+    fi
 fi
