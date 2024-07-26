14c84f4f9d6a8f054d7555eeba58c9fab9d4573d
CoeJoder <CoeJoder@users.noreply.github.com>
2024-07-26 02:30:08 -0700

    Add --update option to deploy script

--- scripts/deploy_setup_unix.sh
+++ scripts/deploy_setup_unix.sh
@@ -2,11 +2,14 @@
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 PROJ_DIR="$(readlink -f "$SCRIPT_DIR/../")"
 
-if [ ~ -ef $PROJ_DIR ]; then
+if [[ ~ -ef $PROJ_DIR ]]; then
     printf "This script is intended to be run from the cloned dir, not the deployment destination.\nExiting...\n"
 else
     echo "Deploying the dotfiles..."
-    rsync -avh --exclude-from="$SCRIPT_DIR/excludes.txt" $PROJ_DIR/ ~
+    if [[ $1 == '--update' ]]; then
+        RSYNC_OPTS='--update'
+    fi
+    rsync -avh $RSYNC_OPTS --exclude-from="$SCRIPT_DIR/excludes.txt" $PROJ_DIR/ ~
 
     echo "Overriding with private configs..."
     GIT_CONFIG=~/.config/git/config
