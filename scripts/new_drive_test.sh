5285f323fa563c06982b5af1656c478a4dde99d8
CoeJoder <CoeJoder@users.noreply.github.com>
2024-09-08 19:36:11 -0700

    Fix new_drive_test.sh

--- scripts/new_drive_test.sh
+++ scripts/new_drive_test.sh
@@ -5,7 +5,7 @@ if [[ -z $DISK ]]; then
 	echo "No disk specified"
 	exit 1
 fi
-BLOCKSIZE=$(sudo blockdev --getbsz /dev/sda)
+BLOCKSIZE=$(sudo blockdev --getbsz $DISK)
 if [[ ! $BLOCKSIZE =~ ^[0-9]+$ ]]; then
 	echo "Invalid block size: ${BLOCKSIZE:-empty}"
 	exit 1
@@ -14,4 +14,3 @@ echo "Block size of $DISK detected as $BLOCKSIZE B"
 read -p "Continue with destructive r/w test? (y/N): " confirm && \
     [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 sudo badblocks -wsvb $BLOCKSIZE $DISK
-
